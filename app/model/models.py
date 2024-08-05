9166258ec9b9a49691e5ab15b21dd4ac05adf5f6
Naved Ahmad Khan <navedkhan8086@gmail.com>
2024-08-05 11:04:32 +0530

    chages in models

--- app/model/models.py
+++ app/model/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine, Column, Integer,String, Boolean, DateTime
+from sqlalchemy import Column, Integer, String, Boolean, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 from datetime import datetime
 
@@ -6,10 +6,10 @@ Base = declarative_base()
 
 class Todo(Base):
     __tablename__ = "todos"
-
-    id = Column(Integer,primary_key=True,index=True)
-    title = Column(str,index=True)
-    description = Column(String,nullable=True)
-    completed = Column(Boolean,default=False)
+    
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, index=True)
+    description = Column(String, nullable=True)
+    completed = Column(Boolean, default=False)
     created_at = Column(DateTime, default=datetime.utcnow)
-    updated_at = Column(DateTime, default=datetime.utcnow,onupdate=datetime.utcnow)
+    updated_at = Column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
